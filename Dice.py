84b8165d189e53a571af415801fec20ea029b64b
MilkManMaki <78377730+MilkManMaki@users.noreply.github.com>
2024-03-12 13:29:55 -0400

    Sprites load. Dice class working.

--- Dice.py
+++ Dice.py
@@ -1,39 +1,42 @@
 from random import randint
+from pygame.sprite import Sprite
 import pygame
 
 
-class Dice():
+class Dice(Sprite):
     """ Dice class"""
 
-    def __init__(self, sides: int, main_game):
+    def __init__(self, main_game):
         """Initiate our dice"""
         self.mysprites = {
-            0: 'sprites/dice-six-faces-none.png',
-            1: 'sprites/dice-six-faces-one.png',
-            2: 'sprites/dice-six-faces-two.png',
-            3: 'sprites/dice-six-faces-three.png',
-            4: 'sprites/dice-six-faces-four.png',
-            5: 'sprites/dice-six-faces-five.png',
-            6: 'sprites/dice-six-faces-six.png',
-            'mixing': 'sprites/rolling-dice-cup.png',
-            'throwing': 'sprites/cubes.png',
+            0: 'sprites/dice-six-faces-none.bmp',
+            1: 'sprites/dice-six-faces-one.bmp',
+            2: 'sprites/dice-six-faces-two.bmp',
+            3: 'sprites/dice-six-faces-three.bmp',
+            4: 'sprites/dice-six-faces-four.bmp',
+            5: 'sprites/dice-six-faces-five.bmp',
+            6: 'sprites/dice-six-faces-six.bmp',
+            'mixing': 'sprites/rolling-dice-cup.bmp',
+            'throwing': 'sprites/cubes.bmp',
         }
-        self.sides = sides
+        super().__init__()
+        self.sides = 6
         self.is_held = False
         self.screen = main_game.screen
-        self.settings = main_game.settings
         self.screen_rect = main_game.screen.get_rect()
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load(self.mysprites[0])
-        self.rect = self.image.get_rect()
-
+        self.rect = self.image.get_rect(center = self.screen.get_rect().center)
+        self.rect.midleft = self.screen_rect.midleft
+        self.x = float(self.rect.x)
 
     def roll(self) -> int:
         """ Roll the dice and return its value """
         if not self.is_held:
             holder = randint(1, self.sides)
             self.image = pygame.image.load(self.mysprites[holder])
+            self.image = pygame.transform.scale(self.image,(200,200))
             return holder
         else:
             return False
@@ -41,3 +44,7 @@ class Dice():
     def hold(self):
         """ Hold the dice so it cannot be rolled"""
         self.is_held = not self.is_held
+
+    def draw_die(self, givenx):
+        """ Draw the dice at its current location """
+        self.screen.blit(self.image, self.rect.move(givenx, 125))
--- DiceGUI.py
+++ DiceGUI.py
@@ -1,6 +1,11 @@
 import pygame
 import sys
+
+from pygame import Color
+
 from Dice import Dice
+
+
 class DiceGUI:
     """ Overall class to manage game assets and behavior """
 
@@ -8,47 +13,74 @@ class DiceGUI:
         """ Initialize the game and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
-
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         pygame.display.set_caption("Dice GUI")
 
+        self.die = pygame.sprite.Group()
+
     def run(self):
         """Start the main loop for the game"""
-        self.sequence()
-        self._check_events()
-        self._update_screen()
-        self.clock.tick(60)
-        pass
+        while True:
+            # self.sequence()
+            self._check_events()
+            #self._update_die()
+            self._update_screen()
+            self.clock.tick(60)
+
     def sequence(self):
-        pass
+        # rolling dice cup move the cup diagonally side to side (Roll function)
+        for dice in self.die:
+            print(dice.roll())
+            if input("Hold?") == 'y':
+                dice.hold()
+
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
+                print("down event")
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
+                print("up event")
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print("Mouse up")
+                pos = pygame.mouse.get_pos()
+
+                # get a list of all sprites that are under the mouse cursor
+                # Find out what die is under the mouse cursur when clicked. Change held to 'true'
+                # clicked_sprites = [s for s in self.die if s.rect.collidepoint(pos)]
+                # print(clicked_sprites)
+
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._next_die()
+
+    def _next_die(self):
+        """ Add new dice to die list. Sprite group """
+        if len(self.die) < 6:
+            new_die = Dice(self)
+            new_die.roll()
+            self.die.add(new_die)
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
-        if event.key == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            # get a list of all sprites that are under the mouse cursor
-            clicked_sprites = [s for s in sprites if s.rect.collidepoint(pos)]
-            # do something with the clicked sprites...
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+        pass
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
-        self.screen.fill('white')
+        self.screen.fill('#FFFDD0')
+        offby = 0
+        for dice in self.die.sprites():
+            dice.draw_die(offby)
+            offby += 273
 
         pygame.display.flip()
 
