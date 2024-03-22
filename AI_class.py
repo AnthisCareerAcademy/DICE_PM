1c7b2a2b345796645104f553f6c0f5e02050ece5
Obeth <142341592+Obeth297@users.noreply.github.com>
2024-03-22 12:51:28 -0400

    Scorecard added
    I added the scorecard along with changing he hand to a list

--- AI_class.py
+++ AI_class.py
@@ -1,9 +1,25 @@
 
+
 class AI:
-    def __init__(self, name, hand):
+    def __init__(self, name):
         self.name = name
-        self.hand = hand
+        self.hand = [0, 0, 0, 0, 0]
         self.held = []
+        self.scorecard = {
+            "Ones": None,
+            "Twos": None,
+            "Threes": None,
+            "Fours": None,
+            "Fives": None,
+            "Sixes": None,
+            "Three of a Kind": None,
+            "Four of a Kind": None,
+            "Full House": None,
+            "Small Straight": None,
+            "Large Straight": None,
+            "Yahtzee": None,
+            "Chance": None
+        }
 
     def check_yahtzee(self):
         same_number = 0
--- obeth.py
+++ obeth.py
@@ -1,9 +1,24 @@
 
 class AI:
-    def __init__(self, name, hand):
+    def __init__(self, name):
         self.name = name
-        self.hand = hand
+        self.hand = [0, 0, 0, 0, 0]
         self.held = []
+        self.scorecard = {
+            "Ones": None,
+            "Twos": None,
+            "Threes": None,
+            "Fours": None,
+            "Fives": None,
+            "Sixes": None,
+            "Three of a Kind": None,
+            "Four of a Kind": None,
+            "Full House": None,
+            "Small Straight": None,
+            "Large Straight": None,
+            "Yahtzee": None,
+            "Chance": None
+        }
 
     def check_yahtzee(self):
         same_number = 0
@@ -18,6 +33,6 @@ class AI:
 
 names = ['Ughene', 'Alpbert', 'Kharleigh', 'Catelenhn']
 
-ughene = AI('Ughene', [5, 5, 5, 5, 5])
+ughene = AI('Ughene')
 
 ughene.check_yahtzee()
