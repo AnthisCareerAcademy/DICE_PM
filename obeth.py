2ad88ce39e96131aea6debe17857cff399a527ec
Martin <142341592+Obeth297@users.noreply.github.com>
2024-03-22 11:36:27 -0400

    AI Class created
    I created the class for AI along with the possible names they can have.

---
+++ obeth.py
@@ -0,0 +1,9 @@
+
+class AI:
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+        self.held = []
+
+
+names = ['Ughene', 'Alpbert', 'Kharleigh', 'Catelenhn']
