b6254304bd376169aa3e1b7a8438cc4db42dad73
Obeth <142341592+Obeth297@users.noreply.github.com>
2024-03-22 11:50:42 -0400

    Added Yahtzee
    I created a function that will allow the AI to check for a Yahtzee when they roll the die.

--- obeth.py
+++ obeth.py
@@ -5,5 +5,19 @@ class AI:
         self.hand = hand
         self.held = []
 
+    def check_yahtzee(self):
+        same_number = 0
+        for die in self.hand:
+            if die == self.hand[0]:
+                same_number += 1
+        if same_number == 5:
+            print("YAHTZEE!")
+        else:
+            print("nothing.")
+
 
 names = ['Ughene', 'Alpbert', 'Kharleigh', 'Catelenhn']
+
+ughene = AI('Ughene', [5, 5, 5, 5, 5])
+
+ughene.check_yahtzee()
